1b046e35e6c5b7a92d71cb64e1d8cf56988dee22
Romain Lalaut <romain@quarkstudio.fr>
2015-04-21 12:36:13 +0000

    Update Pundit::NotAuthorizedError call

--- lib/rails_admin/extensions/pundit/authorization_adapter.rb
+++ lib/rails_admin/extensions/pundit/authorization_adapter.rb
@@ -29,7 +29,7 @@ module RailsAdmin
         def authorize(action, abstract_model = nil, model_object = nil)
           record = model_object || abstract_model && abstract_model.model
           unless policy(record).rails_admin?(action)
-            raise ::Pundit::NotAuthorizedError, "not allowed to #{action} this #{record}"
+            raise ::Pundit::NotAuthorizedError, query: action, record: record
           end
         end
 
